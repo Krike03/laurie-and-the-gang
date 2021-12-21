3879eae7a82aa9692565f478e03d6a63bc1af439
Tom Descheemaeker <tomdescheemaeker@hotmail.com>
2021-12-21 11:32:50 +0100

    feat(Story ME1) : Added Address.java

--- src/main/java/org/laurieandthegang/people/Address.java
+++ src/main/java/org/laurieandthegang/people/Address.java
@@ -13,9 +13,28 @@ public class Address {
 
 
     @ManyToOne
+    @JoinColumn(name = "NUMERALCODE") // should be the same as PostalCode.java id
     private PostalCode postalCode; // could be 9000B
 
 
     private Address() {
     }
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public String getStreetNumber() {
+        return streetNumber;
+    }
+
+    public PostalCode getPostalCode() {
+        return postalCode;
+    }
+
+    @Override
+    public String toString() {
+        return streetName + " " + streetNumber + " " + postalCode;
+    }
 }
+
