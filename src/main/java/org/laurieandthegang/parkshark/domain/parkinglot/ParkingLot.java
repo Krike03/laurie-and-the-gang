f69200989b2e209126beaad8eb1dd8fcf2f4ba9d
tmassart873 <tim.massart@hotmail.com>
2021-12-22 11:58:00 +0100

    feat(PL1) : created ParkingLot class

---
+++ src/main/java/org/laurieandthegang/parkshark/domain/parkinglot/ParkingLot.java
@@ -0,0 +1,82 @@
+package org.laurieandthegang.parkshark.domain.parkinglot;
+
+import org.laurieandthegang.parkshark.domain.people.Address;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "PARKING_LOT")
+public class ParkingLot {
+
+    @Id
+    private int id;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "CATEGORY")
+    private Category category;
+
+    @Column(name = "CAPACITY")
+    private int capacity;
+
+    @ManyToOne
+    @JoinColumn(name = "contact_person_id") // todo to ask the client if its allowed to manage more then 1 parkinglot
+    private ContactPerson contactPerson;
+
+    @Embedded
+    private Address address;
+
+    private ParkingLot() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public ContactPerson getContactPerson() {
+        return contactPerson;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    @Override
+    public String toString() {
+        return "ParkingLot{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", category=" + category +
+                ", capacity=" + capacity +
+                ", contactPerson=" + contactPerson +
+                ", address=" + address +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ParkingLot that = (ParkingLot) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
