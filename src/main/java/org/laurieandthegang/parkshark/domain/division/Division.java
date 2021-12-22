00f29d9bd28c50580c03d07a7937cc54cec70190
Krike03 <deschutter.christophe@gmail.com>
2021-12-22 10:35:06 +0100

    feat (DI1): createTable and class Division

--- src/main/java/org/laurieandthegang/parkshark/domain/division/Division.java
+++ src/main/java/org/laurieandthegang/parkshark/domain/division/Division.java
@@ -1,5 +1,40 @@
 package org.laurieandthegang.parkshark.domain.division;
 
-public class Division {
+import javax.persistence.*;
 
+@Entity
+@Table(name = "DIVISION")
+public class Division {
+    @Id
+    @SequenceGenerator(name = "DIVISION_SEQ", sequenceName = "division_seq", initialValue = 1, allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "DIVISION_SEQ")
+    private int id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "original_name" , nullable = false)
+    private String originalName;
+
+    @Column(name = "director", nullable = false)
+    private String director;
+
+    public Division() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getOriginalName() {
+        return originalName;
+    }
+
+    public String getDirector() {
+        return director;
+    }
 }
--- src/main/resources/db/migration/V2 __Create_division.sql
+++
@@ -1,7 +0,0 @@
-create sequence IF NOT EXISTS division_seq start with 1 increment by 1;
-create table division{
-    id integer default division_seq.nextval primary key,
-    name varchar(255) not null ,
-    original_name varchar(255) not null ,
-    director varchar(255) not null
-    }
