f32e561a20a1eea36fb2990e988eb4d90c715292
tmassart873 <tim.massart@hotmail.com>
2021-12-22 15:53:30 +0100

    feat(PL1) : Contact Person is a true table

--- src/main/resources/db/migration/V4__Create_Contact_Person.sql
+++ src/main/resources/db/migration/V4__Create_Contact_Person.sql
@@ -1,7 +1,7 @@
-create sequence IF NOT EXISTS parking_lot_seq start with 1 increment by 1;
-create table parking_lot
+create sequence IF NOT EXISTS contact_person_seq start with 1 increment by 1;
+create table contact_person
 (
-    id                  integer default nextval('parking_lot_seq') primary key,
+    id                  integer default nextval('contact_person_seq') primary key,
     name                varchar(255) not null,
     email               varchar(255) not null,
     mobile_phone_number varchar(255),
