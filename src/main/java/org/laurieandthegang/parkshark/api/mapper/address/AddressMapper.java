9b6d20fecd9391dceaf4e73edb6c22e586762c56
Tom Descheemaeker <tomdescheemaeker@hotmail.com>
2021-12-23 11:09:23 +0100

    Feat(Story PL1) : AddressMapper.java

--- src/main/java/org/laurieandthegang/parkshark/api/mapper/address/AddressMapper.java
+++ src/main/java/org/laurieandthegang/parkshark/api/mapper/address/AddressMapper.java
@@ -1,4 +1,21 @@
 package org.laurieandthegang.parkshark.api.mapper.address;
 
+import org.laurieandthegang.parkshark.api.dto.address.AddressDto;
+import org.laurieandthegang.parkshark.domain.people.Address;
+import org.springframework.stereotype.Component;
+
+@Component
 public class AddressMapper {
+    private final PostalCodeMapper postalCodeMapper;
+
+    public AddressMapper(PostalCodeMapper postalCodeMapper) {
+        this.postalCodeMapper = postalCodeMapper;
+    }
+
+    public Address mapper(AddressDto addressDto) {
+        return new Address(
+                addressDto.streetName(),
+                addressDto.streetNumber(),
+                postalCodeMapper.mapper(addressDto.postalCode()));
+    }
 }
