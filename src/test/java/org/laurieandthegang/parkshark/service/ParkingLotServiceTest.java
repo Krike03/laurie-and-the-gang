c1d2980d1d5e7f12e47fc3398e443b8860e8f619
Tom Descheemaeker <tomdescheemaeker@hotmail.com>
2021-12-23 13:28:42 +0100

    Fix(Story PL1) : Fixed ParkingLotServiceTest.java; and added V7__Alter_Contact_Person.sql

--- src/test/java/org/laurieandthegang/parkshark/service/ParkingLotServiceTest.java
+++ src/test/java/org/laurieandthegang/parkshark/service/ParkingLotServiceTest.java
@@ -1,13 +1,16 @@
 package org.laurieandthegang.parkshark.service;
 
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.laurieandthegang.parkshark.api.dto.parkinglot.CreateParkingLotDto;
 import org.laurieandthegang.parkshark.api.dto.parkinglot.ParkingLotDto;
 import org.laurieandthegang.parkshark.api.mapper.address.AddressMapper;
+import org.laurieandthegang.parkshark.api.mapper.address.PostalCodeMapper;
 import org.laurieandthegang.parkshark.api.mapper.parkinglot.ContactPersonMapper;
 import org.laurieandthegang.parkshark.api.mapper.parkinglot.ParkingLotMapper;
+import org.laurieandthegang.parkshark.api.mapper.people.LicensePlateMapper;
+import org.laurieandthegang.parkshark.api.mapper.people.NameMapper;
 import org.laurieandthegang.parkshark.domain.parkinglot.Category;
 import org.laurieandthegang.parkshark.domain.parkinglot.ContactPerson;
 import org.laurieandthegang.parkshark.domain.parkinglot.ParkingLot;
@@ -16,26 +19,19 @@ import org.laurieandthegang.parkshark.domain.people.Name;
 import org.laurieandthegang.parkshark.domain.people.PostalCode;
 import org.laurieandthegang.parkshark.repository.ParkingLotRepository;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.mockito.ArgumentMatchers.any;
-
 
 public class ParkingLotServiceTest {
 
     private ParkingLotRepository mockParkingLotRepository;
-
-    @Autowired
     private ParkingLotService parkingLotService;
-
-    @Autowired
     private ParkingLotMapper parkingLotMapper;
-
-    @Autowired
     private ContactPersonMapper contactPersonMapper;
-
-    @Autowired
     private AddressMapper addressMapper;
+    private PostalCodeMapper postalCodeMapper;
+    private NameMapper nameMapper;
+    private LicensePlateMapper licensePlateMapper;
 
     private ContactPerson contactPerson;
     private PostalCode postalCode;
@@ -43,15 +39,24 @@ public class ParkingLotServiceTest {
     private CreateParkingLotDto createParkingLotDto;
 
 
-    @BeforeAll
+    @BeforeEach
     void setUp() {
         mockParkingLotRepository = Mockito.mock(ParkingLotRepository.class);
+        nameMapper = new NameMapper();
+        licensePlateMapper = new LicensePlateMapper();
+        postalCodeMapper = new PostalCodeMapper();
+        addressMapper = new AddressMapper(postalCodeMapper);
+        contactPersonMapper = new ContactPersonMapper(nameMapper, addressMapper);
+        parkingLotMapper = new ParkingLotMapper(contactPersonMapper, addressMapper);
+        parkingLotService = new ParkingLotService(mockParkingLotRepository, parkingLotMapper,
+                new Validator(addressMapper,
+                        licensePlateMapper,
+                        contactPersonMapper));
 
         Address contactPersonsAddress = new Address("Sesams", "123", new PostalCode("123", "BE"));
         contactPerson = new ContactPerson(new Name("Tims", "second mistake"), "tim@2ndmistake.org", "2- 1 - 11 en de rest zoekte zelf", null, contactPersonsAddress);
         postalCode = new PostalCode("9000", "Gent");
         address = new Address("Sint-Pieters NieuwStraat", "8964", postalCode);
-
 
 
         createParkingLotDto = new CreateParkingLotDto(
